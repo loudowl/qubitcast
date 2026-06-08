4ef39b7ff278c37db9f84bd5b7f373cde78265db
Chris <loudowl@mac.com>
2026-06-07 22:25:02 -0700

    feat: initial implementation

---
+++ QubitCast/QubitCastApp/Utilities/Constants.swift
@@ -0,0 +1,9 @@
+import SwiftUI
+
+struct Constants {
+    static let primaryColor = Color("QuantumBlue")
+    static let secondaryColor = Color("QuantumGreen")
+    static let accentColor = Color("SunshineYellow")
+    static let backgroundColor = Color("MidnightBlack")
+    static let textColor = Color("CloudWhite")
+}
