92379e2bb9c6470369568fa4f7733a9bf68643f9
Chris <loudowl@mac.com>
2026-06-07 22:25:01 -0700

    feat: initial implementation

---
+++ QubitCast/QubitCastApp/Utilities/LocationManager.swift
@@ -0,0 +1,19 @@
+import CoreLocation
+
+class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
+    private let locationManager = CLLocationManager()
+    @Published var userLocation: CLLocation?
+
+    override init() {
+        super.init()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else { return }
+        userLocation = location
+    }
+}
